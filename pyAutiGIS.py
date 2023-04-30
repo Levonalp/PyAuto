7b40bffe4ff4b5c7b5d1acd825653fbadda3cda0
levonal <lwshabee@gmail.com>
2023-04-30 15:38:32 -0500

    edit

--- pyAutiGIS.py
+++ pyAutiGIS.py
@@ -13,24 +13,29 @@ center_lon = property_data.geometry.centroid.x.mean()
 property_map = folium.Map(location=[center_lat, center_lon], zoom_start=14)
 
 # Define a function to style the GeoJSON features
+
+
 def style_function(feature):
     return {
-        'fillColor': 'blue',  # or use feature['properties']['your_property_field'] for dynamic color
+        # or use feature['properties']['your_property_field'] for dynamic color
+        'fillColor': 'blue',
         'color': 'black',
         'weight': 2,
         'opacity': 0.5
     }
+
 
 # Add property boundaries to the map
 folium.GeoJson(
     property_data,
     name='Property Boundaries',
     style_function=style_function,
-    tooltip=folium.GeoJsonTooltip(fields=['property_id', 'address'], aliases=['Property ID', 'Address'])
+    tooltip=folium.GeoJsonTooltip(fields=['property_id', 'address'], aliases=[
+                                  'Property ID', 'Address'])
 ).add_to(property_map)
 
 # Add a layer control to toggle property boundaries
 folium.LayerControl().add_to(property_map)
 
-# Save the map as an HTML file
+# Save the map as an HTML files
 property_map.save('property_map.html')
