fea001203403d2efe9f6edf93caeae545fae2bca
levonal <lwshabee@gmail.com>
2023-05-11 17:20:08 -0500

    comments add

--- auto.py
+++ auto.py
@@ -8,20 +8,20 @@ from email.mime.multipart import MIMEMultipart
 email = "your_email@example.com"
 password = "your_password"
 
-# Set the recipient email address
+# Set the recipient email address (for testing purposes, you can use your own email)
 recipient_email = "recipient_email@example.com"
 
-# Download the web page
+# Download the web page using requests
 url = "https://example.com"
 response = requests.get(url)
 
-# Parse the HTML content
+# Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Extract specific information using BeautifulSoup (modify as needed)
 info = soup.find("div", {"class": "some_class"}).get_text()
 
-# Compose the email
+# Compose the email message
 subject = "Extracted Information"
 body = f"Here's the extracted information from {url}:\n\n{info}"
 
@@ -31,7 +31,7 @@ msg["To"] = recipient_email
 msg["Subject"] = subject
 msg.attach(MIMEText(body, "plain"))
 
-# Send the email using your email account
+# Send the email using your email account credentials
 try:
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(email, password)
